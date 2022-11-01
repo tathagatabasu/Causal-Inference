1c82b120b7c1759cc47f8220ada3b248e0ce61a0
tathagatabasu <tathagatabasumaths@gmail.com>
2022-11-01 21:32:57 +0100

    dss addition

---
+++ causal.R
@@ -0,0 +1,162 @@
+library(mvtnorm)
+library(rjags)
+library(SSLASSO)
+library(monomvn)
+library(spikeslab)
+library(horseshoe)
+library(glmnet)
+library(ncvreg)
+library(xtable)
+
+sig1 = diag(100)
+
+x = rmvnorm(50, sigma = sig1)
+
+beta1 = c(runif(5, -4, -1), runif(5, 1, 4))
+gamma1 = c(runif(5, -4, -1), runif(5, 1, 4))
+
+a1_dum = 1/(1 + exp(-x[,1:length(beta1)] %*% gamma1))
+a1 = sapply(1:50, function(j)rbinom(1,1,a1_dum[j]))
+
+
+y1 = 4*a1 + x[,1:length(beta1)] %*% beta1 + rnorm(50, sd = .5)
+
+
+ISVS = function(x, y, a, alphas, tau_0 = 1e-2, tau_1 = 1, gam_a = 1e-2, gam_b = 1e-2, n.iter = 500, n.adapt = 500, n.sample = 5000){
+  MCMC = list()
+  
+  string = "
+  model {
+  
+    ## Likelihood
+    
+    for (i in 1:N) 
+    {
+      mean[i] = b_T * a[i] + inprod(x[i,], b)
+      y[i] ~ dnorm(mean[i], inv.var)
+      
+      a[i] ~ dinterval(a_dumm[i], 0)
+      
+      a_dumm[i] ~ dnorm(inprod(x[i,], g), 1)
+    }
+    
+    # Prior on the mean
+    
+    b_T ~ dnorm(0,inv.var)
+    b0 ~ dnorm(0,inv.var)
+    g0 ~ dnorm(0,1)
+    
+    # prior on the precision
+    
+    inv.var ~ dgamma(gam_a, gam_b)
+    
+    sigma2 = 1 / inv.var
+    
+    ## Prior on the regression parameters 
+    
+    for (j in 1:p) 
+    {
+      # prior on the selection probability
+      w[j] ~ dbeta(2*alpha[j], 2*(1-alpha[j]))I(0.001,0.999)
+      
+      
+      # Prior on beta
+      
+      spike[j] ~ dnorm(0, (inv.var / tau_0^2))
+      slab[j] ~ dnorm(0, (inv.var / tau_1^2))
+      
+      spikeg[j] ~ dnorm(0, (1 / tau_0^2))
+      slabg[j] ~ dnorm(0, (1 / tau_1^2))
+      
+      b[j] = (1 - w[j]) * slab[j]  + w[j] * spike[j]
+      g[j] = (1 - w[j]) * slabg[j]  + w[j] * spikeg[j]
+    }
+    
+  }
+  "
+  params = c("b", "b0", "b_T", "g", "g0", "w", "sigma2")
+  
+  for (i in 1:length(alphas)) {
+    
+    alpha = as.vector(rep(alphas[i], ncol(x)))
+    
+    ISVS <- textConnection(string, open = "r")
+    
+    data = list(
+      y = as.vector(y),
+      x = x,
+      a = a,
+      p = ncol(x),
+      N = nrow(x),
+      alpha = alpha,
+      gam_a = gam_a,
+      gam_b = gam_b,
+      tau_0 = tau_0,
+      tau_1 = tau_1
+    )
+    
+    model <- jags.model(data = data, file = ISVS, n.chains = 1, n.adapt = n.adapt)
+    update(model, n.iter)
+    MCMC[[i]] = coda.samples(model, params, n.sample)
+  }
+  
+  b_post = list()
+  b0_post = list()
+  b_T_post = list()
+  g_post = list()
+  g0_post = list()
+  w_post = list()
+  
+  for (i in 1:length(alphas)) {
+    
+    b_post[[i]] = sapply(1:data$p, function(j)as.matrix(MCMC[[i]][,j]))
+    
+    b0_post[[i]] = MCMC[[i]][,1+data$p]
+    
+    b_T_post[[i]] = MCMC[[i]][,2+data$p]
+    
+    g_post[[i]] = sapply(1:data$p, function(j)as.matrix(MCMC[[i]][,j+data$p+2]))
+    
+    g0_post[[i]] = MCMC[[i]][,2*data$p+3]
+    
+    w_post[[i]] = sapply(1:data$p, function(j)as.matrix(MCMC[[i]][,(j+2*data$p+4)]))
+    
+  }
+  
+  output = list("MCMC" = MCMC, "Betas" = b_post, "Beta_int" = b0_post, 
+                "probs" = w_post, "Causal_post" = b_T_post, "Gammas" = g_post, "Gamma_int" = g0_post)
+  
+  return(output)
+  
+}
+
+sim1_rbvs1 = ISVS(x, y1, a1, alphas = seq(0.8,0.9, length.out = 2), tau_1 = 1)
+
+prob_exp = matrix(unlist(lapply(sim1_rbvs1$probs, function(x)colMeans(as.matrix(x)))), nrow = 2, byrow = T)
+
+active = c(1:100)[-c(which(apply(prob_exp, 2, min)>0.5))] 
+
+beta_exp = matrix(unlist(lapply(sim1_rbvs1$Betas, function(x)colMeans(as.matrix(x)))), nrow = 2, byrow = T)
+
+gamma_exp = matrix(unlist(lapply(sim1_rbvs1$Gammas, function(x)colMeans(as.matrix(x)))), nrow = 2, byrow = T)
+
+beta0_exp = matrix(unlist(lapply(sim1_rbvs1$Beta_int, function(x)colMeans(as.matrix(x)))), nrow = 2, byrow = T)
+
+gamma0_exp = matrix(unlist(lapply(sim1_rbvs1$Gamma_int, function(x)colMeans(as.matrix(x)))), nrow = 2, byrow = T)
+
+beta_dss = c()
+gamma_dss = c()
+
+for (i in 1:2) {
+  beta_dss = rbind(beta_dss, coef(cv.glmnet(x[,active], x[,active]%*%beta_exp[i,active], penalty.factor = 1/abs(beta_exp[i,active]), intercept = F))[-1])
+  gamma_dss = rbind(gamma_dss, coef(cv.glmnet(x[,active], x[,active]%*%gamma_exp[i,active], penalty.factor = 1/abs(gamma_exp[i,active]), intercept = F))[-1])
+}
+
+causal_est = c()
+
+for (i in 1:2) {
+  y_new = y1 - x[,active] %*% beta_dss[i,]
+  T_new = a1 - (pnorm(x[,active] %*% gamma_dss[i,]))
+  
+  causal_est = rbind(causal_est, (t(T_new)%*%T_new)^(-1)%*%t(T_new)%*%y_new)
+}
