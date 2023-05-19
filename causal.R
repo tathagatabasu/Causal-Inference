c83f81c3e969a4316a0b400c6eec5a08372ddbea
tathagatabasu <tathagatabasumaths@gmail.com>
2023-05-19 17:03:46 +0100

    update code for par

--- causal.R
+++ causal.R
@@ -7,8 +7,10 @@ library(horseshoe)
 library(glmnet)
 library(ncvreg)
 library(xtable)
+library(foreach)
+library(doParallel)
 
-set.seed(1e3)
+set.seed(1e8)
 
 sig1 = diag(100)
 
@@ -24,7 +26,7 @@ a1 = sapply(1:50, function(j)rbinom(1,1,a1_dum[j]))
 y1 = 4*a1 + x[,1:length(beta1)] %*% beta1 + rnorm(50, sd = .1)
 
 
-ISVS = function(x, y, a, alphas, tau_0 = 1e-5, tau_1 = 10, gam_a = 1e-2, gam_b = 1e-2, n.iter = 1000, n.adapt = 500, n.sample = 1000){
+ISVS = function(x, y, a, alphas, tau_0 = 1e-5, tau_1 = 10, gam_a = 1e-2, gam_b = 1e-2, n.iter = 10000, n.adapt = 5000, n.sample = 10000, n.cores = 5){
   MCMC = list()
   
   string = "
@@ -70,15 +72,18 @@ ISVS = function(x, y, a, alphas, tau_0 = 1e-5, tau_1 = 10, gam_a = 1e-2, gam_b =
       spikeg[j] ~ dnorm(0, (1 / tau_0^2))
       slabg[j] ~ dnorm(0, (1 / tau_1^2))
       
-      b[j] = (1 - w[j]) * slab[j]  + w[j] * spike[j]
-      g[j] = (1 - w[j]) * slabg[j]  + w[j] * spikeg[j]
+      b[j] = w[j] * slab[j]  + (1 - w[j]) * spike[j]
+      g[j] = w[j] * slabg[j]  + (1 - w[j]) * spikeg[j]
     }
     
   }
   "
   params = c("b", "b0", "b_T", "g", "g0", "w", "sigma2")
   
-  for (i in 1:length(alphas)) {
+  cl = makeCluster(n.cores) #not to overload your computer
+  registerDoParallel(cl)
+  
+  MCMC = foreach (i = 1:length(alphas), .packages = 'rjags') %dopar% {
     
     alpha = as.vector(alphas[i])
     
@@ -99,8 +104,10 @@ ISVS = function(x, y, a, alphas, tau_0 = 1e-5, tau_1 = 10, gam_a = 1e-2, gam_b =
     
     model <- jags.model(data = data, file = ISVS, n.chains = 1, n.adapt = n.adapt)
     update(model, n.iter)
-    MCMC[[i]] = coda.samples(model, params, n.sample)
+    coda.samples(model, params, n.sample)
   }
+  
+  stopCluster(cl)
   
   b_post = list()
   b0_post = list()
@@ -111,17 +118,17 @@ ISVS = function(x, y, a, alphas, tau_0 = 1e-5, tau_1 = 10, gam_a = 1e-2, gam_b =
   
   for (i in 1:length(alphas)) {
     
-    b_post[[i]] = sapply(1:data$p, function(j)as.matrix(MCMC[[i]][,j]))
+    b_post[[i]] = as.matrix(MCMC[[i]][[1]])[,1:ncol(x)]
     
-    b0_post[[i]] = MCMC[[i]][,1+data$p]
+    b0_post[[i]] = as.matrix(MCMC[[i]][[1]])[,(1+ncol(x))]
     
-    b_T_post[[i]] = MCMC[[i]][,2+data$p]
+    b_T_post[[i]] = as.matrix(MCMC[[i]][[1]])[,(2+ncol(x))]
     
-    g_post[[i]] = sapply(1:data$p, function(j)as.matrix(MCMC[[i]][,j+data$p+2]))
+    g_post[[i]] = as.matrix(MCMC[[i]][[1]])[,(ncol(x)+3):(2*ncol(x)+2)]
     
-    g0_post[[i]] = MCMC[[i]][,2*data$p+3]
+    g0_post[[i]] = as.matrix(MCMC[[i]][[1]])[,2*ncol(x)+3]
     
-    w_post[[i]] = sapply(1:data$p, function(j)as.matrix(MCMC[[i]][,(j+2*data$p+4)]))
+    w_post[[i]] = as.matrix(MCMC[[i]][[1]])[,(2*ncol(x)+5):(3*ncol(x)+4)]
     
   }
   
@@ -129,47 +136,42 @@ ISVS = function(x, y, a, alphas, tau_0 = 1e-5, tau_1 = 10, gam_a = 1e-2, gam_b =
                 "probs" = w_post, "Causal_post" = b_T_post, "Gammas" = g_post, "Gamma_int" = g0_post)
   
   return(output)
-  
 }
 
-sim1_rbvs1 = ISVS(x, y1, a1, alphas = seq(0.8,0.9, length.out = 5), tau_1 = 5)
+sim1_rbvs1 = ISVS(x, y1, a1, alphas = seq(0.1,0.2, length.out = 21), tau_1 = 1, n.cores = 7)
 
-prob_exp = matrix(unlist(lapply(sim1_rbvs1$probs, function(x)colMeans(as.matrix(x)))), nrow = 5, byrow = T)
+prob_exp = matrix(unlist(lapply(sim1_rbvs1$probs, function(x)colMeans(as.matrix(x)))), nrow = length(sim1_rbvs1$Causal_post), byrow = T)
 
-active = t(sapply(1:5, function(i) (prob_exp[i,] <= 0.5)))
-  #c(1:100)[-c(which(apply(prob_exp, 2, min)>0.5))] 
+active = t(sapply(1:length(sim1_rbvs1$Causal_post), function(i) (prob_exp[i,] > 0.5)))
 
-beta_exp = matrix(unlist(lapply(sim1_rbvs1$Betas, function(x)colMeans(as.matrix(x)))), nrow = 5, byrow = T)
+beta_exp = matrix(unlist(lapply(sim1_rbvs1$Betas, function(x)colMeans(as.matrix(x)))), nrow = length(sim1_rbvs1$Causal_post), byrow = T)
 
-gamma_exp = matrix(unlist(lapply(sim1_rbvs1$Gammas, function(x)colMeans(as.matrix(x)))), nrow = 5, byrow = T)
+gamma_exp = matrix(unlist(lapply(sim1_rbvs1$Gammas, function(x)colMeans(as.matrix(x)))), nrow = length(sim1_rbvs1$Causal_post), byrow = T)
 
-beta0_exp = matrix(unlist(lapply(sim1_rbvs1$Beta_int, function(x)colMeans(as.matrix(x)))), nrow = 5, byrow = T)
+beta0_exp = matrix(unlist(lapply(sim1_rbvs1$Beta_int, function(x)colMeans(as.matrix(x)))), nrow = length(sim1_rbvs1$Causal_post), byrow = T)
 
-Causal_exp = matrix(unlist(lapply(sim1_rbvs1$Causal_post, function(x)colMeans(as.matrix(x)))), nrow = 5, byrow = T)
+Causal_exp = matrix(unlist(lapply(sim1_rbvs1$Causal_post, function(x)colMeans(as.matrix(x)))), nrow = length(sim1_rbvs1$Causal_post), byrow = T)
 
-gamma0_exp = matrix(unlist(lapply(sim1_rbvs1$Gamma_int, function(x)colMeans(as.matrix(x)))), nrow = 5, byrow = T)
+gamma0_exp = matrix(unlist(lapply(sim1_rbvs1$Gamma_int, function(x)colMeans(as.matrix(x)))), nrow = length(sim1_rbvs1$Causal_post), byrow = T)
 
 beta_dss = list()
 gamma_dss = list()
 
-for (i in 1:5) {
+for (i in 1:length(sim1_rbvs1$Causal_post)) {
   beta_dss[[i]] = as.vector(coef(cv.glmnet(x[,which(active[i,] ==T)], x[,which(active[i,] ==T)] %*% beta_exp[i,which(active[i,] ==T)], penalty.factor = 1/abs(beta_exp[i,which(active[i,] ==T)]), intercept = F))[-1])
   gamma_dss[[i]] = as.vector(coef(cv.glmnet(x[,which(active[i,] ==T)], x[,which(active[i,] ==T)] %*%gamma_exp[i,which(active[i,] ==T)], penalty.factor = 1/abs(gamma_exp[i,which(active[i,] ==T)]), intercept = F))[-1])
 }
 
 causal_corr = c()
 
-for (i in 1:5) {
-  y_new = y1 - x[,which(active[i,] ==T)] %*% beta_dss[[i]] - beta0_exp[i]
-  print(sum(y_new))
-  T_new = a1 - ((pnorm(x[,which(active[i,] ==T)] %*% gamma_dss[[i]] + gamma0_exp[i]))) 
-  if (sum(T_new) == 0)
-    dumm_corr = 0
-  else
-    dumm_corr = (t(T_new)%*%T_new)^(-1)%*%t(T_new)%*%y_new
+for (i in 1:length(sim1_rbvs1$Causal_post)) {
   
-  causal_corr = rbind(causal_corr, dumm_corr)
+  T_new = matrix(as.numeric((pnorm(x[,which(active[i,] ==T)] %*% gamma_dss[[i]] + gamma0_exp[i]))>0.5), ncol = 1)
+  
+  causal_corr[i] = (t(T_new)%*%T_new)^(-1)%*%t(T_new)%*%(Causal_exp[i] * matrix(as.numeric((pnorm(x %*% gamma_exp[i,] + gamma0_exp[i]))>0.5), ncol = 1) + x %*% beta_exp[i,] - x[,which(active[i,] ==T)] %*% beta_dss[[i]])
+  
 }
 Causal_exp
 
 causal_corr
+
